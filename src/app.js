ccc0ce189e3133eb44c89bcbd8fa2e4a23a46413
Peyush Nuwal <piyushnawal19@gmail.com>
2025-09-25 21:28:51 +0530

    feat(api): added absolute import

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import logger from '#config/logger';
 
 const app = express();
 
 app.get('/', (req, res) => {
+  logger.info('hello from winston');
   res.status(200).send('hello from acquisitions');
 });
 
