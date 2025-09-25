782c2264989d700e5c0f193570057cff492323c7
Peyush Nuwal <piyushnawal19@gmail.com>
2025-09-25 20:55:14 +0530

    feat(api):  created user modal

---
+++ src/models/user.model.js
@@ -0,0 +1,11 @@
+import { pgTable, serial, timestamp, varchar } from 'drizzle-orm/pg-core';
+
+export const users = pgTable('user', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 255 }).notNull(),
+  email: varchar('email', { length: 255 }).unique().notNull(),
+  password: varchar('password', { length: 255 }).notNull(),
+  role: varchar('role', { length: 50 }).notNull().default('user'),
+  created_at: timestamp().defaultNow().notNull(),
+  updated_at: timestamp().defaultNow().notNull(),
+});
