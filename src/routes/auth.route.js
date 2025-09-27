42f8fa0af456df7652b17a3c7c38af361a09df68
Peyush Nuwal <piyushnawal19@gmail.com>
2025-09-27 23:00:16 +0530

    feat(api): change sign-in route from GET to POST method

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -5,7 +5,7 @@ import { signUp, signIn, signOut } from '#controllers/auth.controller.js';
 const router = express.Router();
 
 router.post('/sign-up', signUp);
-router.get('/sign-in', signIn);
+router.post('/sign-in', signIn);
 router.get('/sign-out', signOut);
 
 export default router;
